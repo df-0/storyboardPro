d613d927da8453675c3904f3b037ea3ac77ad9ea
df-0 <dave@rgl.tv>
2024-07-11 14:27:40 +0100

    ah, how do i group

--- collectAndHideCurrent.js
+++ collectAndHideCurrent.js
@@ -9,6 +9,31 @@ function CollectAndHide()
     var layM = new LayerManager;
     var sM = new StoryboardManager;
 
+    var selectionManager = new SelectionManager();
+
+    var panelIds = selectionManager.getPanelSelection();
+    if (panelIds.length != 1) {
+        MessageLog.trace('make sure only one panel is selected');
+        return;
+    }
+
+    var currentPanelId = panelIds[0];
+    MessageLog.trace(currentPanelId);
+    var visibleLayers = []
+    for ( var j =  layM.numberOfLayers( currentPanelId ) - 1; j >= 0; --j )
+    {
+    if ( layM.layerVisibility( currentPanelId, j ) == true )
+    {
+        //  delLayers += (" Scene: " + sM.nameOfScene( sM.sceneIdOfPanel( currentPanelId ) ) 
+        //            + "    Panel: " + sM.nameOfPanel( currentPanelId ) 
+        //            + "    Layer: " + layM.layerName( currentPanelId, j ) 
+        //            + "\n" );
+        visibleLayers.push(layM.layerName( currentPanelId, j ));
+            //layM.deleteLayer( currentPanelId, j );
+    }
+    MessageLog.trace(visibleLayers);
+    }
+
     // for ( var i = 0; i < selIds.length; ++ i )
     //     {
     //         var panelId = selIds[i];
