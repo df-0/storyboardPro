f309eaa04b9fbff609f923e6b5617b362e484720
df-0 <dave@rgl.tv>
2024-07-11 15:31:47 +0100

    kinda works

--- collectAndHideCurrent.js
+++ collectAndHideCurrent.js
@@ -18,34 +18,42 @@ function CollectAndHide()
     }
 
     var currentPanelId = panelIds[0];
-    MessageLog.trace(currentPanelId);
     var visibleLayers = []
     // get a list of all the visible layers
-    for ( var j =  layM.numberOfLayers( currentPanelId ) - 1; j >= 0; --j )
+    for ( var j =  0; j < layM.numberOfLayers( currentPanelId ); j++ )
     {
-    if ( layM.layerVisibility( currentPanelId, j ) == true )
-    {
-        //  delLayers += (" Scene: " + sM.nameOfScene( sM.sceneIdOfPanel( currentPanelId ) ) 
-        //            + "    Panel: " + sM.nameOfPanel( currentPanelId ) 
-        //            + "    Layer: " + layM.layerName( currentPanelId, j ) 
-        //            + "\n" );
-        visibleLayers.push(layM.layerName( currentPanelId, j ));
+        if ( layM.layerVisibility( currentPanelId, j ) == true )
+        {
+            //  delLayers += (" Scene: " + sM.nameOfScene( sM.sceneIdOfPanel( currentPanelId ) ) 
+            //            + "    Panel: " + sM.nameOfPanel( currentPanelId ) 
+            //            + "    Layer: " + layM.layerName( currentPanelId, j ) 
+            //            + "\n" );
+            visibleLayers.push(layM.layerName( currentPanelId, j ));
             //layM.deleteLayer( currentPanelId, j );
+        }
     }
-
+    
     // file:///C:/Program%20Files%20(x86)/Toon%20Boom%20Animation/Toon%20Boom%20Storyboard%20Pro%2024/help/storyboard/classLayerManager.html#a85bbc8fb35167cd5a94b21992bb0f2b9
-    MessageLog.trace("adding group layer");
-    addGroupLayer (currentPanelId, layM.numberOfLayers( currentPanelId ) - 1, false, "version_000"); // add a group layer
-    MessageLog.trace("Done!");
+    layM.addGroupLayer (currentPanelId, layM.numberOfLayers(currentPanelId) - 1, false, "version"); // add a group layer
+    var groupName = layM.layerName(currentPanelId, layM.numberOfLayers(currentPanelId)-1);
 
-    // move all the layers into the group layer
-    // hide all the layers
-    // rename all the layers
-    // create new blank versions of the layers
+    for (var iLayer = 0; iLayer < visibleLayers.length; iLayer++)
+    {
+        var iCurrentLayer = layM.layerIndexFromName(currentPanelId, visibleLayers[iLayer]);
+        var iGroup = layM.layerIndexFromName(currentPanelId, groupName);
+        layM.setLayerVisible(currentPanelId, iCurrentLayer, false);// hide all the layers
+        layM.moveLayerInGroup(currentPanelId, iCurrentLayer, iGroup);  // move all the layers into the group layer - invalidates index!
+        // rename all the layers
+        // create new blank versions of the layers
+    }
 
     
-    MessageLog.trace(visibleLayers);
-    }
+    
+
+    
+
+    
+
 
     // for ( var i = 0; i < selIds.length; ++ i )
     //     {
